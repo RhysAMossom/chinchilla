26f0db4880cefc30a97f450d40736bb7b4442670
Test Engineering <test_engineer@TestEngineeringNotebook.(none)>
2013-12-12 19:36:00 -0500

    adding optoparse to LED simulator, using LEDs drawing for searchlight

--- OpenScad/LED_search_light/LED_search_light.scad
+++ OpenScad/LED_search_light/LED_search_light.scad
@@ -1,4 +1,5 @@
 use </home/chinchillabot/chinchilla/OpenScad/cc_library.scad>
+use </home/test_engineer/chinchilla/OpenScad/cc_library.scad>
 
 l = 40;
 w = 40;
@@ -8,15 +9,13 @@ difference(){
 	// main cube
 
 	translate([0,0,1]) round_corner_square(l,w,h,2);
-	
-	for (x = [2.5:5:l]){
-			translate([x,2.5,0])
-				led_3mm();
-	}		
-	for (x = [2.5:5:35]){
-			translate([x,7.5,0])
-				led_3mm();
-	}	
 
+	for (x = [2.5:5:l]){
+		for (y = [2.5:5:w]){
+			translate([x,y,0])
+				//cylinder(r=1.6, h = 10);
+				led_3mm();
+		}
+	}
 
 }
--- Python/LED_strip_simulator/LED_Strip_Sim.py
+++ Python/LED_strip_simulator/LED_Strip_Sim.py
@@ -1,22 +1,37 @@
+#! /usr/bin/env python
 import sys
 import optparse
 import pygame
 
 if __name__ == "__main__":
+	parser = optparse.OptionParser()
+	parser.add_option("-l", "--leds", dest = "leds", default=60, help="Number of LEDs")
+	parser.add_option("-W", "--width", dest = "width", default=680, help="Windows width")
+	parser.add_option("-H", "--height", dest = "height", default=480, help="Windows height")
+	(options, args) = parser.parse_args()
+	
+	
 	pygame.init() 
-
+	
 	#create the screen
-	window = pygame.display.set_mode((640, 480)) 
+	window = pygame.display.set_mode((options.width, options.height)) 
 
 	#draw a line - see http://www.pygame.org/docs/ref/draw.html for more 
 	pygame.draw.line(window, (255, 255, 255), (0, 0), (30, 50))
+	
+	pixel_w = options.width/options.leds
+	pixel_h = pixel_w
+	
+	for x in range(0,options.width, options.width):
+		pygame.draw.rect(window, (0,0,0), (0,0), (x,pixel_h))
+	
 	#draw it to the screen
 	pygame.display.flip() 
 
 	#input handling (somewhat boilerplate code):
 	while True: 
 	   for event in pygame.event.get(): 
-	      if event.type == pygame.QUIT: 
-	          sys.exit(0) 
-	      else: 
-	          print event 
+		  if event.type == pygame.QUIT: 
+			  sys.exit(0) 
+		  else: 
+			  print event 
